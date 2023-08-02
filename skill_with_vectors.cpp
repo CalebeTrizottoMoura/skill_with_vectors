7ea9bf384aba1f07e448ea06b0a4b83bbf665f85
Calebe Moura <contatocalebemoura@gmail.com>
2023-08-02 15:34:12 -0300

    added_functions_1.0.3

--- skill_with_vectors.cpp
+++ skill_with_vectors.cpp
@@ -3,6 +3,67 @@
 #include <climits>
 #include <iomanip>
 
+double calculator_average_age(double average_age, int sum_age, int numb_people){
+    average_age = (double) sum_age / numb_people;
+    return average_age;
+}
+
+double calculator_average_height(double average_height, double sum_height, int numb_people){
+    average_height = sum_height / numb_people;
+    return average_height;
+}
+
+double calculator_percentage_femenine(double percentage_femenine, int sum_femenine, int numb_people){
+    percentage_femenine = (double) sum_femenine / numb_people * 100;
+    return percentage_femenine;
+}
+
+double calculator_percentage_masculine(double percentage_masculine, int sum_masculine, int numb_people){
+    percentage_masculine = (double) sum_masculine / numb_people * 100;
+    return percentage_masculine;
+}
+
+void procedure_sum_gender(char array_gender, int *sum_femenine, int *sum_masculine, bool *control_sum_gender){
+    if(array_gender == 'F' || array_gender == 'f'){
+        (*sum_femenine)++;
+        *control_sum_gender = false;
+    }else if(array_gender == 'M' || array_gender == 'm'){
+        (*sum_masculine)++;
+        *control_sum_gender = false;
+    }else{
+        std::cout << "Por favor, digite 'F' para feminino ou 'M' para masculino.\n";
+    }
+}
+
+void people_data(int numb_people, int *code, int *sum_femenine, int *sum_masculine, int *sum_age, double *sum_height,
+                 std::string *array_name, char *array_gender, int *array_age, double *array_height){      
+    bool control_sum_gender = true; 
+    for(int i = 0; i < numb_people; i++){
+        std::cin.ignore(INT_MAX, '\n');
+        code[i] = i + 1000;
+
+        std::cout << "========= DADOS DA " << i + 1 << "ª PESSOA =========\n";
+        std::cout << "Nome: ";
+        getline(std::cin, array_name[i]);
+
+        do{
+            std::cout << "Sexo (F/M): ";
+            std::cin >> array_gender[i];
+
+            procedure_sum_gender(array_gender[i], sum_femenine, sum_masculine, &control_sum_gender);
+        }while(control_sum_gender);
+
+        std::cout << "Idade: ";
+        std::cin >> array_age[i];
+        *sum_age += array_age[i];
+        
+        std::cout << "Altura: ";
+        std::cin >> array_height[i];
+        *sum_height += array_height[i];
+        system("cls");
+    }
+}
+
 void main_menu(int *option){
     std::cout << "========= MENU PRINCIPAL =========\n";
     std::cout << "1 - Listar dados\n";
@@ -16,9 +77,10 @@ void main_menu(int *option){
     system("cls");
 }
 
-void data_list(int numb_peaple, std::string array_name[], char array_gender[], int array_age[], double array_height[], int code[]){
+void data_list(int numb_people, std::string array_name[], char array_gender[], int array_age[], double array_height[], 
+               int code[]){
     std::cout << "========= LISTAGEM DE DADOS =========\n";
-    for(int i = 0; i < numb_peaple; i++){
+    for(int i = 0; i < numb_people; i++){
         std::cout << "------------- " << i + 1 << "ª PESSOA -------------\n";
         std::cout << "Nome: " << array_name[i] << std::endl;
         std::cout << "Sexo: " << array_gender[i] << std::endl;
@@ -113,7 +175,6 @@ void age_change(std::string array_name, char array_gender, int *array_age, doubl
 }
 
 void height_change(std::string array_name, char array_gender, int array_age, double *array_height, int *cont_new){
-    std::cin.ignore(INT_MAX, '\n');
     std::cout << "========= ALTERAR ALTURA =========\n";
     std::cout << "Nome: " << array_name << std::endl;
     std::cout << "Sexo: " << array_gender << std::endl;
@@ -130,56 +191,29 @@ void height_change(std::string array_name, char array_gender, int array_age, dou
 
 int main(){
     setlocale(LC_ALL, "portuguese");
-
-    int numb_peaple;
-    std::cout << "========= MENU INICIAL =========\n";
-    std::cout << "Quantas pessoas farão parte do estudo? ";
-    std::cin >> numb_peaple;
-
-    int code[numb_peaple], sum_age = 0, sum_masculine = 0, sum_femenine = 0, array_age[numb_peaple];
-    std::string array_name[numb_peaple];
-    double array_height[numb_peaple], sum_height = 0;
-    char array_gender[numb_peaple];
-    system("cls");
-
-    for(int i = 0; i < numb_peaple; i++){
-        std::cin.ignore(INT_MAX, '\n');
-        code[i] = i + 1000;
-
-        std::cout << "========= DADOS DA " << i + 1 << "ª PESSOA =========\n";
-        std::cout << "Nome: ";
-        getline(std::cin, array_name[i]);
-
-        do{
-            std::cout << "Sexo (F/M): ";
-            std::cin >> array_gender[i];
-
-            if(array_gender[i] == 'F'){
-                sum_femenine = sum_femenine + 1;
-            }else if(array_gender[i] == 'M'){
-                sum_masculine = sum_masculine + 1;
-            }else{
-                std::cout << "Por favor, digite 'F' para feminino ou 'M' para masculino.\n";
-            }
-        }while(array_gender[i] != 'F' && array_gender[i] != 'M');
-
-        std::cout << "Idade: ";
-        std::cin >> array_age[i];
-        sum_age += array_age[i];
-        
-        std::cout << "Altura: ";
-        std::cin >> array_height[i];
-        sum_height += array_height[i];
-        system("cls");
-    }
-
-    double average_age = (double) sum_age / numb_peaple;
-    double average_height = sum_height / numb_peaple;
-
-    double percentage_femenine = ((double) sum_femenine / numb_peaple) * 100;
-    double percentage_masculine = ((double) sum_masculine / numb_peaple) * 100;
-
     std::cout << std::fixed << std::setprecision(2);
+
+    std::cout << "========= MENU INICIAL =========\n";
+    std::cout << "Quantas pessoas farão parte da análise? ";
+    int numb_people;
+    std::cin >> numb_people;
+
+    int sum_age = 0, sum_masculine = 0, sum_femenine = 0, array_age[numb_people], code[numb_people];
+    std::string array_name[numb_people];
+    double array_height[numb_people], sum_height = 0, average_age = 0, average_height = 0, percentage_femenine = 0, 
+           percentage_masculine = 0;
+    char array_gender[numb_people];
+    system("cls");
+    
+    people_data(numb_people, code, &sum_femenine, &sum_masculine, &sum_age, &sum_height, array_name, array_gender,
+                array_age, array_height);
+
+    average_age = calculator_average_age(average_age, sum_age, numb_people);
+    average_height = calculator_average_height(average_height, sum_height, numb_people);
+
+    percentage_femenine = calculator_percentage_femenine(percentage_femenine, sum_femenine, numb_people);
+    percentage_masculine = calculator_percentage_masculine(percentage_masculine, sum_masculine, numb_people);
+
     std::cin.ignore(INT_MAX, '\n');
 
     int option = 0, change_code = 0, internal_option = 0, cont = 0, cont_new = 0;
@@ -187,16 +221,17 @@ int main(){
     do{
         if(cont > 0 && internal_option != 5){
             system("pause");
+        }else{
+            internal_option = 0;
         }
         system("cls");
         cont++;
-        internal_option = 0;
 
         main_menu(&option);
 
         switch(option){
             case 1:
-                data_list(numb_peaple, array_name, array_gender, array_age, array_height, code);
+                data_list(numb_people, array_name, array_gender, array_age, array_height, code);
                 break;
             case 2:
                 age_average(average_age);
@@ -222,7 +257,7 @@ int main(){
                     
                     switch(internal_option){
                         case 1:                     
-                            for(int i = 0; i < numb_peaple; i++){
+                            for(int i = 0; i < numb_people; i++){
                                 if(code[i] == change_code){
                                     name_change(&array_name[i], array_gender[i], array_age[i], array_height[i], &cont_new);
                                     break; 
@@ -230,41 +265,48 @@ int main(){
                             }
                             break;
                         case 2: 
-                            for(int i = 0; i < numb_peaple; i++){
+                            for(int i = 0; i < numb_people; i++){
                                 if(code[i] == change_code){
                                     gender_change(array_name[i], &array_gender[i], array_age[i], array_height[i], &cont_new);
+                                    //correção
+                                    sum_femenine = 0;
+                                    sum_masculine = 0;
+                                    bool control_sum_gender = true;
+
+                                    procedure_sum_gender(array_gender[i], &sum_femenine, &sum_masculine, &control_sum_gender);
+
+                                    percentage_femenine = calculator_percentage_femenine(percentage_femenine, sum_femenine, numb_people);
+                                    percentage_masculine = calculator_percentage_masculine(percentage_masculine, sum_masculine, numb_people);
                                     break; 
                                 }
                             }
                             break;
                         case 3: 
-                            for(int i = 0; i < numb_peaple; i++){
+                            for(int i = 0; i < numb_people; i++){
                                 if(code[i] == change_code){
                                     age_change(array_name[i], array_gender[i], &array_age[i], array_height[i], &cont_new);
 
                                     int sum_new_age = 0;
-                                    for(int i = 0; i < numb_peaple; i++){
+                                    for(int i = 0; i < numb_people; i++){
                                         sum_new_age += array_age[i];
                                     }
 
-                                    double average_new_age = (double) sum_new_age / numb_peaple;
-                                    average_age = average_new_age;                        
+                                    average_age = calculator_average_age(average_age, sum_new_age, numb_people);                      
                                     break; 
                                 }
                             }
                             break;
                         case 4: 
-                            for(int i = 0; i < numb_peaple; i++){
+                            for(int i = 0; i < numb_people; i++){
                                 if(code[i] == change_code){
                                     height_change(array_name[i], array_gender[i], array_age[i], &array_height[i], &cont_new);
 
-                                    double sum_new_height;
-                                    for(int i = 0; i < numb_peaple; i++){
+                                    double sum_new_height = 0;
+                                    for(int i = 0; i < numb_people; i++){
                                         sum_new_height += array_height[i];
                                     }
 
-                                    double average_new_height = sum_new_height / numb_peaple;
-                                    average_height = average_new_height;
+                                    average_height = calculator_average_height(average_height, sum_new_height, numb_people); 
                                     break; 
                                 }
                             }
